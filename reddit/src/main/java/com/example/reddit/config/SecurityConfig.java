b9d8e2450b47d49315a23aaecf519f25477f08f8
Jean Camargos <jeancamargosdev@gmail.com>
2023-05-04 09:00:26 -0300

    user registration finished

--- reddit/src/main/java/com/example/reddit/config/SecurityConfig.java
+++ reddit/src/main/java/com/example/reddit/config/SecurityConfig.java
@@ -1,66 +1,45 @@
 package com.example.reddit.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.cors().and()
-                .csrf().disable()
-                .authorizeHttpRequests()
+         httpSecurity
+                        .httpBasic()
+                        .and()
+                        .authorizeHttpRequests()
                         .requestMatchers("/api/auth/**")
-                        .permitAll()
-                        .anyRequest()
+                        .permitAll().anyRequest()
                         .authenticated()
-                .and().build();
-}
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity.cors().and()
-//                .csrf().disable()
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/api/auth/**")
-//                        .permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/subreddit")
-//                        .permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/posts/")
-//                        .permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/posts/**")
-//                        .permitAll()
-//                        .requestMatchers("/v2/api-docs",
-//                                "/configuration/ui",
-//                                "/swagger-resources/**",
-//                                "/configuration/security",
-//                                "/swagger-ui.html",
-//                                "/webjars/**")
-//                        .permitAll()
-//                        .anyRequest()
-//                        .authenticated())
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .exceptionHandling(exceptions -> exceptions
-//                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-//                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-//                ).build();
-//    }
-    @Bean
+                        .and()
+                        .csrf().disable();
+                return httpSecurity.build();
+}    @Bean
         PasswordEncoder passwordEncoder(){
-            return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
-
-
-
 }
+
+//Deprecated:
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//    @Override
+//    public void configure (HttpSecurity httpSecurity) throws Exception {
+//         httpSecurity.csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/api/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated();
+//}
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
--- reddit/src/main/java/com/example/reddit/controller/AuthController.java
+++ reddit/src/main/java/com/example/reddit/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
         authService.signup(registerRequest);
-        return new ResponseEntity<>("User Registration Successfull", HttpStatus.OK);
+        return new ResponseEntity<>("User Registration Successfull ", HttpStatus.OK);
 
     }
 }
