0fefd2b8bae918dc1c7d20b5c097b6fc2d7762cc
Jean Camargos <jeancamargosdev@gmail.com>
2023-06-16 09:43:05 -0300

    erro de cors policy

--- angular-reddit-clone/src/app/auth/login/login.component.ts
+++ angular-reddit-clone/src/app/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginRequestPayload } from './login.request.payload';
 import { AuthService } from '../shared/auth.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +13,12 @@ import { AuthService } from '../shared/auth.service';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  // loginForm?: FormGroup;
   loginRequestPayload: LoginRequestPayload;
-  // isError: boolean;
+  isError?: boolean;
+  registerSuccessMessage?: string;
  
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute, private router: Router, private toastr: ToastrService) {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
@@ -31,6 +35,15 @@ export class LoginComponent implements OnInit {
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     })
+
+    this.activatedRoute.queryParams
+    .subscribe(params => {
+      if (params['registered'] !== undefined && params['registered'] === 'true') {
+        this.toastr.success('Signup Successful');
+        this.registerSuccessMessage = 'Please Check your inbox for activation email '
+          + 'activate your account before you Login!';
+      }
+    });
     
   }
 
@@ -39,7 +52,13 @@ export class LoginComponent implements OnInit {
     this.loginRequestPayload.password = this.loginForm.get('password')?.value;
 
     this.authService.login(this.loginRequestPayload).subscribe(data => {
-      console.log('Login successful');
+      if (data) {
+        this.isError = false;
+        this.router.navigateByUrl('/');
+        this.toastr.success('Login Successful');
+      } else {
+        this.isError = true;
+      }
     });
   }
 
